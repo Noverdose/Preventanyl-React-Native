e3fd451a9a27da48948a952782d76c585a3d6733
Yudhvir Raj <rajyudhvir29@gmail.com>
2018-02-25 05:30:39 -0800

    Able to dismiss and call an action when action button is pressed for genericpopupdialog

--- preventanyl/src/utils/GenericPopupDialog.js
+++ preventanyl/src/utils/GenericPopupDialog.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, View } from 'react-native';
 
-import PopupDialog, { DialogTitle } from 'react-native-popup-dialog';
+import PopupDialog, { DialogTitle, DialogButton } from 'react-native-popup-dialog';
+
+const DIALOG_HEIGHT = 0.3;
+const DIALOG_WIDTH  = 0.75;
+const DIALOG_LEFT_BUTTON_TEXT = "CANCEL";
+const DISMISS_TOUCH_OUTSIDE = false;
 
 export default class GenericPopupDialog extends Component {
 
@@ -13,13 +18,22 @@ export default class GenericPopupDialog extends Component {
         this.popupDialog.show ();
     }
 
+    dismiss () {
+        this.popupDialog.dismiss ();
+    }
+
     render () {
         return (
             <PopupDialog
                 dialogTitle = { <DialogTitle title = { this.props.title } /> }
                 ref = { (popupDialog) => { this.popupDialog = popupDialog; }} 
-                width = { 0.5 }
-                height = { 0.5 } >
+                width = { DIALOG_WIDTH }
+                height = { DIALOG_HEIGHT }
+                dismissOnTouchOutside = { DISMISS_TOUCH_OUTSIDE }
+                actions = { [
+                    <DialogButton key = { 1 } text = { DIALOG_LEFT_BUTTON_TEXT } align = "left" onPress = { () => this.popupDialog.dismiss() } />,
+                    <DialogButton key = { 2 } text = { this.props.actionButtonText } align = "right" onPress = { () => this.props.actionFunction () } />,
+                    ] } >
                 <View>
                     <Text>{ this.props.message }</Text>
                 </View>
