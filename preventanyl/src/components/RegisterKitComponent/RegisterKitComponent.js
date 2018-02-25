30dc52b705811be3000328dbe126006e3c3d2f3a
Yudhvir Raj <rajyudhvir29@gmail.com>
2018-02-25 01:47:26 -0800

    Reset the RegisterKitComponent

--- preventanyl/src/components/RegisterKitComponent/RegisterKitComponent.js
+++ preventanyl/src/components/RegisterKitComponent/RegisterKitComponent.js
@@ -1,39 +1,11 @@
 import React, { Component } from 'react';
-import { AppRegistry, Button, Text, View } from 'react-native';
-
-import PopupDialog from 'react-native-popup-dialog';
+import { AppRegistry, Text, View } from 'react-native';
 
 export default class RegisterKitComponent extends Component {
-
-  constructor () {
-    super ();
-    this.state = {
-      alertText : "ABC"
-    }
-  }
 
   render () {
     return (
       <View>
-        <Button
-          title="Show Dialog"
-          onPress={() => {
-            this.popupDialog.show();
-          }} />
-        <PopupDialog
-          ref={(popupDialog) => { this.popupDialog = popupDialog; }}
-        >
-      <View>
-        <Text>{ this.state.alertText }</Text>
-        <Button
-          title="Change text"
-          onPress={() => {
-            this.setState ({
-              alertText : "123"
-            })
-          }} />
-      </View>
-      </PopupDialog>
           <Text>Register Kit Component</Text>
       </View>
     );
