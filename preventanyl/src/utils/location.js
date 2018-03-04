2320acc05ff07cbdb101c7aeea3e09c77962995d
Yudhvir Raj <rajyudhvir29@gmail.com>
2018-03-04 14:29:16 -0800

    Added to location file in utils

--- preventanyl/src/utils/location.js
+++ preventanyl/src/utils/location.js
@@ -4,6 +4,29 @@ export const getCurrentLocation = (successCallback, failureCallback) => {
     }).then (result => {
         successCallback (result);
     }).catch (error => {
-        failureCallback (result);
+        failureCallback (error);
     });
 };
+
+export const convertLocationToLatitudeLongitude = (location) => {
+    /*  Object {
+            "coords": Object {
+                "accuracy": 65,
+                "altitude": 109.47386169433594,
+                "altitudeAccuracy": 10,
+                "heading": -1,
+                "latitude": 49.22724996030237,
+                "longitude": -122.92167081498236,
+                "speed": -1,
+            },
+            "timestamp": 1519656714765.542,
+        } */
+
+    return {
+        latlng : {
+            latitude  : location.coords.latitude,
+            longitude : location.coords.longitude,
+        },
+        error : null,
+    }
+}
