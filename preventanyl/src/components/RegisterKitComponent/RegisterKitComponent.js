d0259a6f9ad2d3f76ba5c5d0bc8addfd19326488
Yudhvir Raj <rajyudhvir29@gmail.com>
2018-03-10 20:32:52 -0800

    Form fields ui setup, will change ui after, not seperate as of yet

--- preventanyl/src/components/RegisterKitComponent/RegisterKitComponent.js
+++ preventanyl/src/components/RegisterKitComponent/RegisterKitComponent.js
@@ -1,15 +1,38 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import { AppRegistry, KeyboardAvoidingView, ScrollView, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
+
+import DismissKeyboard from 'dismissKeyboard';
+
+import RegisterKitForm from './RegisterKitForm';
 
 export default class RegisterKitComponent extends Component {
 
   render () {
     return (
-      <View>
-          <Text>Register Kit Component</Text>
-      </View>
+      <KeyboardAvoidingView behavior = 'padding' style = { styles.container } onPress = { () => { DismissKeyboard () } } >
+        <TouchableWithoutFeedback onPress = { () => { DismissKeyboard () } }>
+            <ScrollView keyboardDismissMode = 'on-drag' >
+                <RegisterKitForm />
+            </ScrollView>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     );
   }
+
 }
+
+const styles = StyleSheet.create ({
+  container : {
+      flex : 1,
+      backgroundColor : '#3498db'
+  },
+  title : {
+    color : '#FFF',
+    marginTop : 10,
+    width : 160,
+    textAlign : 'center',
+    opacity : 0.9
+  }
+});
 
 AppRegistry.registerComponent ('RegisterKitComponent', () => RegisterKitComponent);
