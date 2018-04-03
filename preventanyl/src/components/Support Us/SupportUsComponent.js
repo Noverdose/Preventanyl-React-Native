0668d83c382c5069e67a569870c8be1e2fb6ad4a
Yudhvir Raj <rajyudhvir29@gmail.com>
2018-04-02 21:01:07 -0700

    Cleaned up linking urls, and support us component

--- preventanyl/src/components/Support Us/SupportUsComponent.js
+++ preventanyl/src/components/Support Us/SupportUsComponent.js
@@ -1,46 +1,40 @@
 import React, { Component } from 'react';
-import { AppRegistry, KeyboardAvoidingView, ScrollView, StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
+import { AppRegistry, KeyboardAvoidingView, ScrollView, StyleSheet, Text, TouchableWithoutFeedback, View, WebView } from 'react-native';
 
 import DismissKeyboard from 'dismissKeyboard';
-import {WebView, Linking} from 'react-native';
 
+import { openWebPage } from '../../utils/linkingUrls';
 
 export default class SupportUsComponent extends Component {
 
-  render () {
-      const uri = 'https://www.paypal.me/preventanylApp';
+    render () {
+        const uri = 'https://www.paypal.me/preventanylApp';
 
-      return (
-          <WebView
-              ref={(ref) => { this.webview = ref; }}
-              source={{ uri }}
-              onNavigationStateChange={(event) => {
-                  if (event.url !== uri) {
-                      this.webview.stopLoading();
-                      Linking.openURL(event.url);
-                  }
-              }}
-          />
-       // <View style = { styles.container } >
-       //     <Text>SupportUsComponent</Text>
-       // </View>
-    );
-  }
+        return (
+            <WebView
+                ref = { (ref) => 
+                    { 
+                        this.webview = ref; 
+                    }
+                }
+
+                source = { 
+                    {
+                        uri 
+                    }
+                }
+
+                onNavigationStateChange = { (event) => 
+                    {
+                        if (event.url !== uri) {
+                            this.webview.stopLoading();
+                            openWebPage (event.url);
+                        }
+                    }
+                } />
+        );
+    }
 
 }
 
-const styles = StyleSheet.create ({
-  container : {
-      flex : 1,
-      backgroundColor : '#3498db'
-  },
-  title : {
-    color : '#FFF',
-    marginTop : 10,
-    width : 160,
-    textAlign : 'center',
-    opacity : 0.9
-  }
-});
-
-AppRegistry.registerComponent ('RegisterKitComponent', () => RegisterKitComponent);
+AppRegistry.registerComponent ('SupportUsComponent', () => SupportUsComponent);
