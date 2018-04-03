0e4e452e8cbc5fe3e86b68ab18485b866476947f
Yudhvir Raj <rajyudhvir29@gmail.com>
2018-04-02 23:20:52 -0700

    Updated genericAlerts, more abstracted, updated from Responder

--- preventanyl/src/utils/genericAlerts.js
+++ preventanyl/src/utils/genericAlerts.js
@@ -1,13 +1,33 @@
 import { Alert } from 'react-native';
 
-const ANGELS_TITLE = "Notifying";
-const ANGELS_MESSAGE = "Nearby helpers have been notified";
-const ERROR_TITLE = "Whoops!";
-const REQUIRED_FIELD_TITLE = "Required Field";
-const RESEND_EMAIL = "Resend email";
-const DEFAULT_TITLE = "TITLE";
-const DEFAULT_MESSAGE = "MESSAGE";
-const OKAY = "Okay";
+const ANGELS_TITLE          = "Notifying";
+const ANGELS_MESSAGE        = "Nearby helpers have been notified";
+const ERROR_TITLE           = "Whoops!";
+const REQUIRED_FIELD_TITLE  = "Required Field";
+const RESEND_EMAIL          = "Resend email";
+const DEFAULT_TITLE         = "TITLE";
+const DEFAULT_MESSAGE       = "MESSAGE";
+const CONFIRMATION_TITLE    = "Confirmation required";
+const OKAY                  = "Okay";
+const ACCEPT                = "Accept";
+const CANCEL                = "Cancel";
+
+export const GENERIC_ALERT_OBJECTS = Object.freeze (
+    {
+        OKAY : {
+            text    : OKAY,
+            onPress : () => {}
+        },
+        CANCEL : {
+            text    : CANCEL,
+            onPress : () => {}
+        },
+        UNDISSMISSABLE : {
+            cancelable : false,
+            onDismiss  : () => {}
+        },
+    }
+)
 
 export const genericAlert = (title, message) => {
 
@@ -17,14 +37,9 @@ export const genericAlert = (title, message) => {
     Alert.alert (
         title, message, 
         [
-            {
-                text : OKAY,
-                onPress : () => {}
-            },
-            {
-                cancelable : false
-            }
-        ]
+            GENERIC_ALERT_OBJECTS.OKAY
+        ],  
+        
     )   
 
 }
@@ -71,4 +86,21 @@ export const genericVerificationAlert = (title, message) => {
 
 export const genericDefaultAlert = () => {
     genericAlert (DEFAULT_TITLE, DEFAULT_MESSAGE)
+}
+
+export const genericVerificationAlert = (title, message) => {
+    if (title === "" || message === "")
+        return;
+
+    if (!Database.checkUserVerfied ()) {
+        Alert.alert (
+            title,
+            message,
+            [
+                GENERIC_ALERT_OBJECTS.RESEND_EMAIL,
+                GENERIC_ALERT_OBJECTS.OKAY
+            ],
+            GENERIC_ALERT_OBJECTS.UNDISSMISSABLE
+        );
+    }
 }
