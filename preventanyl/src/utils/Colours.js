04a360117661cb8945f3924967eed559d0bf7694
Yudhvir Raj <rajyudhvir29@gmail.com>
2018-04-07 18:03:14 -0700

    Added blue to colours class

--- preventanyl/src/utils/Colours.js
+++ preventanyl/src/utils/Colours.js
@@ -4,7 +4,8 @@ export default class Colours {
 
     static HEX_COLOURS = Object.freeze (
         {
-            "BLACK" : "#000000"
+            "BLACK" : "#000000",
+            "BLUE"  : "#0000FF"
         }
     )
 
